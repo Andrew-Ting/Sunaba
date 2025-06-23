8ef9a13b20f04f9ee0b6aa2724785d797606af07
Andrew Ting <andrewjamesting@yahoo.com>
2025-06-23 00:27:02 -0700

    Add descriptorlayoutbuilder helper

--- src/vk_descriptors.h
+++ src/vk_descriptors.h
@@ -36,3 +36,12 @@ private:
 	uint32_t mSetsPerPool;
 
 };
+
+struct DescriptorLayoutBuilder {
+
+	std::vector<VkDescriptorSetLayoutBinding> bindings;
+
+	void add_binding(uint32_t binding, VkDescriptorType type);
+	void clear();
+	VkDescriptorSetLayout build(VkDevice device, VkShaderStageFlags shaderStages, void* pNext = nullptr, VkDescriptorSetLayoutCreateFlags flags = 0);
+};
