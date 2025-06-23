4f07dd0a2ba3b50b47d1adff8ef21f24bdc2c611
Andrew Ting <andrewjamesting@yahoo.com>
2025-06-23 00:06:22 -0700

    Add useful util for reading shader files

--- src/vk_utils.h
+++ src/vk_utils.h
@@ -3,6 +3,7 @@
 #include <volk.h>
 
 namespace vkutil {
+	bool load_shader_module(const char* filePath, VkDevice device, VkShaderModule* outShaderModule);
 	void transition_image(VkCommandBuffer cmd, VkImage image, int mipMapLevels, VkImageLayout currentLayout, VkImageLayout newLayout);
 	void copy_image_to_image(VkCommandBuffer cmd, VkImage source, VkImage destination, VkExtent2D srcSize, VkExtent2D dstSize);
 }
